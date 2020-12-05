a9d8f7a09acfbbc6edbceb55c65e45872f8d01d7
Nathaniel May <nate.may@fauna.com>
2020-12-05 12:12:41 -0500

    refactor to use megaparsec

--- src/Filenames.hs
+++ src/Filenames.hs
@@ -1,53 +1,40 @@
 
 module Filenames where
 
-import Control.Applicative ((<|>))
-import Data.Char (isDigit, isUpper, isAlpha)
-import Data.Either (lefts, rights)
+import Data.Foldable (toList)
 import Data.Text (Text)
 import qualified Data.Text as T
-import Data.Text.Read (decimal)
+import Data.Void (Void)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
 
 
-data Tok 
-    = Tok Text | Id | Count Int
+type Parser = Parsec Void Text
+
+data Tok
+    = Tag Text | Id Text | Count Int
     deriving (Eq, Show, Read)
 
-idChars :: Char -> Bool
-idChars '0' = False
-idChars c   = isDigit c || (isAlpha c && isUpper c)
+pTag :: [Text] -> Parser Tok
+pTag schema = do
+    t <- choice $ string <$> schema
+    pure (Tag t)
 
-lexId :: Text -> Maybe (Tok, Text)
-lexId str = 
-    if T.length str == 6 && T.all idChars str
-    then Just (Id, str) 
-    else Nothing
+pId :: Parser Tok
+pId = do
+    fid <- count 6 (choice $ char <$> T.unpack "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789")
+    pure . Id $ T.pack fid
 
-lexCount :: Text -> Maybe (Tok, Text)
-lexCount str = do
-    (i, extra) <- fromEither num
-    if T.null extra
-    then Just (Count i, str)
-    else Nothing
-    where 
-        num = decimal str
-        fromEither (Right x) = Just x
-        fromEither (Left _) = Nothing
+pCount :: Parser Tok
+pCount = do 
+    i <- L.decimal :: Parser Int
+    pure (Count i)
 
-tokenize :: [Text] -> Text -> Either Text (Tok, Text)
-tokenize schema str = 
-    if elem str schema
-    then Right (Tok str, str)
-    else maybe 
-        (Left $ "lexing error: " <> str <> " is not a valid tag, id or count")
-        Right 
-        (lexId str <|> lexCount str)
-    
-lex :: [Text] -> Text -> Either [Text] [(Tok, Text)]
-lex schema str =
-    if null errors
-    then Right (rights results)
-    else Left errors
-    where
-        results = tokenize schema <$> T.split (=='-') str
-        errors = lefts results
+pFilename :: [Text] -> Parser [Tok]
+pFilename schema = do
+    tags  <- some $ (pTag schema) <* char '-'
+    setId <- pId
+    i     <- optional (char '-' *> pCount)
+    _     <- eof 
+    pure $ tags <> [setId] <> (toList i)
--- test/Main.hs
+++ test/Main.hs
@@ -4,6 +4,7 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import Prelude hiding (lex)
 import Test.HUnit
+import Text.Megaparsec (parse)
 import System.Exit
 
 import Filenames
@@ -18,21 +19,21 @@ schema = [
 
 test1 :: Test
 test1 = TestCase $ assertEqual 
-  "Lexes schema concatinated" 
-  (Right [(Tok "abc","abc"), (Tok "de","de"), (Tok "fghi","fghi"), (Tok "j","j")])
-  (lex schema (T.intercalate "-" schema))
+  "Lexes schema concatinated with numeric id" 
+  (Right [Tag "abc",Tag "de",Tag "fghi",Tag "j",Id "123324"])
+  (parse (pFilename schema) "unit-test" "abc-de-fghi-j-123324")
 
 test2 :: Test
 test2 = TestCase $ assertEqual 
-  "Lexes schema concatinated with id and counter" 
-  (Right [(Tok "abc","abc"),(Tok "de","de"),(Tok "fghi","fghi"),(Tok "j","j"),(Id,"ABCDEF")]) 
-  (lex schema ((T.intercalate "-" schema) <> "-ABCDEF"))
+  "Lexes schema concatinated with alpha id" 
+  (Right [Tag "abc",Tag "fghi",Tag "j",Id "ABCDEF"])
+  (parse (pFilename schema) "unit-test" "abc-fghi-j-ABCDEF")
 
 test3 :: Test
 test3 = TestCase $ assertEqual 
-  "Lexes schema concatinated with id and counter" 
-  (Right [(Tok "abc","abc"),(Tok "de","de"),(Tok "fghi","fghi"),(Tok "j","j"),(Id,"ABCDEF"),(Count 3,"3")]) 
-  (lex schema ((T.intercalate "-" schema) <> "-ABCDEF-3"))
+  "Lexes schema concatinated with alpha id and counter" 
+  (Right [Tag "abc",Tag "de",Tag "fghi",Tag "de",Tag "j",Tag "j",Id "ABCDEF",Count 3])
+  (parse (pFilename schema) "unit-test" "abc-de-fghi-de-j-j-ABCDEF-3")
 
 allTests :: [Test]
 allTests = [test1, test2, test3]
