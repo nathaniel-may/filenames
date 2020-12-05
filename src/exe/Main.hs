b646be677643333371a44a418240afda1969a54c
Nathaniel May <nate.may@fauna.com>
2020-12-05 18:47:32 -0500

    moved exe functions to library

--- src/exe/Main.hs
+++ src/exe/Main.hs
@@ -1,27 +1,6 @@
 module Main where
 
-import Options.Applicative
-
-data Input = Input
-  { schema :: FilePath
-  , dir    :: FilePath }
-
-input :: Parser Input
-input = Input
-      <$> argument str
-          ( metavar "SCHEMA-FILE"
-         <> help "Schema file describing name structure" )
-      <*> argument str
-          ( metavar "DIR"
-         <> help "Directory to check filenames" )
+import qualified Exe
 
 main :: IO ()
-main = run =<< execParser opts
-  where
-    opts = info (input <**> helper)
-      ( fullDesc
-     <> progDesc "Parse filenames according to schema file"
-     <> header ":::: filename parser ::::" )
-
-run :: Input -> IO ()
-run (Input _ _) = putStrLn "haven't hooked up the app to the interface yet"
+main = Exe.main
---
+++ src/lib/Exe.hs
@@ -0,0 +1,27 @@
+module Exe where
+
+import Options.Applicative
+
+data Input = Input
+  { schema :: FilePath
+  , dir    :: FilePath }
+
+input :: Parser Input
+input = Input
+      <$> argument str
+          ( metavar "SCHEMA-FILE"
+         <> help "Schema file describing name structure" )
+      <*> argument str
+          ( metavar "DIR"
+         <> help "Directory to check filenames" )
+
+main :: IO ()
+main = run =<< execParser opts
+  where
+    opts = info (input <**> helper)
+      ( fullDesc
+     <> progDesc "Parse filenames according to schema file"
+     <> header ":::: filename parser ::::" )
+
+run :: Input -> IO ()
+run (Input _ _) = putStrLn "haven't hooked up the app to the interface yet"
