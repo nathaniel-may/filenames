de535bf6ff7fd9e37aad173538753a946a3d734a
Nathaniel May <nate.may@fauna.com>
2020-12-06 13:14:23 -0500

    added flag for showing pretty parse errors.

--- src/lib/Exe.hs
+++ src/lib/Exe.hs
@@ -1,23 +1,28 @@
 module Exe where
 
-import           Data.Either         (isLeft)
 import           Data.Text
 import qualified Data.Text as T
 import           Data.Text.IO
+import           Data.Void           (Void)
 import           Filenames           hiding (Parser)
 import           Options.Applicative
 import           Prelude             hiding (readFile, putStrLn, lines)
 import           System.Directory
-import           Text.Megaparsec     (parse)
+import           Text.Megaparsec
 
 data Input = Input
-  { schema :: FilePath
-  , dir    :: FilePath }
+  { details :: Bool 
+  , schema  :: FilePath
+  , dir     :: FilePath }
 
 -- definitions for input
 input :: Parser Input
 input = Input
-      <$> argument str
+      <$> switch
+          ( long "details"
+         <> short 'd'
+         <> help "Whether to show individual parse errors" )
+      <*> argument str
           ( metavar "SCHEMA-FILE"
          <> help "Schema file describing name structure" )
       <*> argument str
@@ -35,12 +40,18 @@ main = run =<< execParser opts
 
 -- where file IO and parsing is done
 run :: Input -> IO ()
-run (Input s d) = do
+run (Input dFlag s d) = do
+    putStrLn ""
     filenames' <- listDirectory d
     let filenames = T.pack <$> filenames'
     schema' <- readFile s
     let tags = lines schema'
     let result = mapM (parse (pFilename tags) "main") filenames
-    if isLeft result
-    then putStrLn "One of the files doesn't match the schema. Can't tell you which one yet."
-    else putStrLn "Everything matches the schema!"
+    if dFlag
+    then either printParseError (\_ -> putStrLn "Everything matches the schema!") result
+    else putStrLn "Eventually this will print a list of filenames that don't match." -- TODO
+
+printParseError :: ParseErrorBundle Text Void -> IO()
+printParseError bundle = do 
+    putStrLn "-mismatching filename found-"
+    putStrLn . T.pack $ errorBundlePretty bundle
