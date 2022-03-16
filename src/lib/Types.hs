231cb6e58802c1c8ad3f9d030e4ec9ce73a169b5
Nathaniel May <nathanieldmay@gmail.com>
2022-03-15 20:56:25 -0400

    delete comment in favor of readme

--- src/lib/Types.hs
+++ src/lib/Types.hs
@@ -57,21 +57,3 @@ instance Display Type where
     display (FnTag ret []) = display ret
     display (FnTag ret params) = foldr (<>) "" symbols where
         symbols = interleave (display <$> params) (repeat " -> ") <> [display ret]
-
-{-
-
-d := "-"
-
-format := with_delim d [
-    no_delim (==1) ["art", "photo"]
-  , delim d (>=0) ["nature", "architecture", "people"]
-  , id
-  , no_delim (>=0) ["1", "2", "3", "4",  "5", "6", "7", "8", "9"]
-  ]
-
-id := no_delim (==6) id_chars
-
-// limited characters for example
-id_chars := ["A", "B", "C", "D", "E", "F", "1", "2", "3"]
-
--}
