37d00f11b0500eecbfcd07c05854e51c16a8a344
Nathaniel May <nathanieldmay@gmail.com>
2022-03-23 01:25:52 -0400

    add lambda typecheck test

--- src/unit/TypeCheckerTests.hs
+++ src/unit/TypeCheckerTests.hs
@@ -1,11 +1,22 @@
 module TypeCheckerTests (tests) where
 
-import CustomPrelude -- import all
-import Exceptions    (TypeException(..))
-import Test.HUnit    -- import all
-import Types         -- import all
-import TypeChecker   -- import all
+import           CustomPrelude -- import all
+import           Data.String   (String)
+import qualified Data.Text     as T
+import           Exceptions    (CompilationException(..), TypeException(..))
+import           Parsers       (parse)
+import           Test.HUnit    -- import all
+import           Types         -- import all
+import           TypeChecker   -- import all
 
+assertRight :: Display a => String -> Either a b -> Assertion
+assertRight title (Left err) = assertString (title <> "\n" <> T.unpack (display err))
+assertRight title _ = assertBool title True
+
+typecheckSource :: Text -> Either CompilationException ExprT
+typecheckSource source = do
+    parsed <- mapLeft ParseErr (parse source)
+    mapLeft TypeErr (typecheck parsed)
 
 test1 :: Test
 test1 = TestCase $ assertEqual 
@@ -34,5 +45,10 @@ test4 = TestCase $ assertEqual
   (Right $ FnCallT (Name "<") BoolTag [IntT 1, IntT 2])
   (typecheck $ FnCallU (Name "<") [IntU 1, IntU 2])
 
+test5 :: Test
+test5 = TestCase $ assertRight 
+  "lambda with builtin '<' typechecks"
+  (typecheckSource "format := []\n lambda := \\x y => x < y")
+
 tests :: [Test]
-tests = [test1, test2, test3, test4]
+tests = [test1, test2, test3, test4, test5]
